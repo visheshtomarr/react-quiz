5e397d58a99e37f094556fd5d760a34665f783d0
visheshtomarr <visheshtomar7800@gmail.com>
2025-05-18 23:29:06 +0530

    Loading questions from our local server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+const initialState = {
+  questions: {},
+  // Status can be in following states: "loading", "error", "ready", 
+  // "active", "finished"
+  status: 'loading'
+};
+
+function reducer(currState, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...currState,
+        questions: action.payload,
+        status: 'ready'
+      };
+
+    case 'dataFailed':
+      return {
+        ...currState,
+        status: 'error'
+      }
+
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch("http://localhost:5000/questions");
+        const data = await res.json();
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (error) {
+        dispatch({ type: 'dataFailed' });
+      }
+    }
+    fetchData();
+  }, [])
+
   return (
     <div className="app">
       <Header />
