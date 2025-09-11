e1f8b072da6a4ef8440f7f72bb94df153fd9908b
visheshtomarr <visheshtomar7800@gmail.com>
2025-09-11 23:51:45 +0530

    Refactored cicd

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className='app-header'>
       <img src='logo512.png' alt='React logo' />
-      <h1>The React Quiz</h1>
+      <h1>The React Quiz V3</h1>
     </header>
   );
 }
